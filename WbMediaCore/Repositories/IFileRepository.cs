837396f54bec2e3a5a1eb8cb0f4ea90b357a5d26
Jonathan Boileau <jonathan.boileau173@gmail.com>
2020-04-27 13:10:41 +0200

    ADD: Suppression via un guid sur le repository

--- WbMediaCore/Repositories/IFileRepository.cs
+++ WbMediaCore/Repositories/IFileRepository.cs
@@ -5,5 +5,7 @@ namespace WbMediaCore.Repositories
     public interface IFileRepository : IGenericRepository<File>
     {
         File GetByGuid(string guid);
+
+        void DeleteByGuid(string guid);
     }
 }
--- WbMediaRepository/Repositories/FileRepository.cs
+++ WbMediaRepository/Repositories/FileRepository.cs
@@ -10,6 +10,14 @@ namespace WbMediaRepository.Repositories
         public FileRepository(Context context) : base(context)
         { }
 
+        public void DeleteByGuid(string guid)
+        {
+            File existing = _table.FirstOrDefault(f => f.Guid == guid);
+            _table.Remove(existing);
+
+            Save();
+        }
+
         public File GetByGuid(string guid)
         {
             return _table.FirstOrDefault(f => f.Guid == guid);
