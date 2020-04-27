ad58b0f164e38e6627d08858f823fc68fb29f8e8
Jonathan Boileau <jonathan.boileau173@gmail.com>
2020-04-27 13:18:39 +0200

    ADD : Fonctionnalité de suppression d'un fichier via son guid

---
+++ WbMediaCore/Features/DeleteFileByGuidFeature/DeleteFileByGuidFeature.cs
@@ -0,0 +1,31 @@
+﻿using System.Transactions;
+using WbMediaCore.Messages;
+using WbMediaCore.Services;
+
+namespace WbMediaCore.Features.DeleteFileByGuidFeature
+{
+    public class DeleteFileByGuidFeature : IFeature<DeleteFileByGuidRequest, DeleteFileByGuidResponse>
+    {
+        public DeleteFileByGuidResponse Result { get; set; }
+
+        private IFileService _fileService { get; set; }
+
+        public DeleteFileByGuidFeature(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        public void Execute(DeleteFileByGuidRequest request)
+        {
+            using (TransactionScope scope = new TransactionScope())
+            {
+                _fileService.DeleteByGuid(request.guid);
+                _fileService.Save();
+
+                scope.Complete();
+            }
+
+            Result = new DeleteFileByGuidResponse();
+        }
+    }
+}
